ff345fbf12f01c291b4c1d010f3cce51fc67ebaf
Zhiyuan Dong <windybeing@sjtu.edu.cn>
2021-10-28 12:22:13 +0800

    refine Example.java to produce much less bytecode

--- example/Example.java
+++ example/Example.java
@@ -1,11 +1,14 @@
 public class Example {
     public static void main(String[] args) {
+        Integer a = new Integer(1);
+        Integer b = new Integer(2);
+
         System.startConcolic();
-        int a = 1;
-        int b = 2;
-        int c = a + b;
-        System.out.println(c);
-        System.symbolize(new Integer(6));
+        System.symbolize(a);
+
+        Integer c = a + b;
         System.endConcolic();
+
+        System.out.println(c);
     }
 }
