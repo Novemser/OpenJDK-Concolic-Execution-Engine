a00666957f9d51cb9ad75e881f06d2ccb955260d
Zhiyuan Dong <windybeing@sjtu.edu.cn>
2021-11-04 10:52:23 +0800

    support type convert

--- hotspot/src/share/vm/concolic/SymbolicOp.hpp
+++ hotspot/src/share/vm/concolic/SymbolicOp.hpp
@@ -25,18 +25,26 @@ enum SymbolicOp {
   op_shl = 18,
   op_shr = 19,
   op_ushr = 20,
+  op_2i = 21,
+  op_2f = 22,
+  op_2l = 23,
+  op_2d = 24,
+  op_2b = 25,
+  op_2c = 26,
+  op_2s = 27,
   op_num,
   op_null
 };
 static const SymbolicOp NotSymbolicOp[op_num] = {
-    op_null, op_null, op_null, op_null, op_null, op_null,
-    op_null, op_null, op_ge,   op_le,   op_gt,   op_lt,
-    op_ne,   op_eq,   op_null, op_null, op_null, op_null,
-    op_null, op_null, op_null};
+    op_null, op_null, op_null, op_null, op_null, op_null, op_null,
+    op_null, op_ge,   op_le,   op_gt,   op_lt,   op_ne,   op_eq,
+    op_null, op_null, op_null, op_null, op_null, op_null, op_null,
+    op_null, op_null, op_null, op_null, op_null, op_null, op_null};
 
 static const char *SymbolicOpStr[op_num] = {
-    "+", "-",  "*",  "&",  "|",  "^", "/",   "%",    "<",
-    ">", "<=", ">=", "==", "!=", "-", "cmp", "cmpl", "cmpg","<<",">>",">>u"};
+    "+",   "-",  "*",  "&",  "|",  "^",   "/",    "%",    "<",  ">",
+    "<=",  ">=", "==", "!=", "-",  "cmp", "cmpl", "cmpg", "<<", ">>",
+    ">>u", "2i", "2f", "2l", "2d", "2b",  "2c",   "2s"};
 
 #endif
 
