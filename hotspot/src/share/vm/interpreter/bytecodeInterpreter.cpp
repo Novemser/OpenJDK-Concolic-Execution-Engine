b45924679fe8d7e913bacc65933333da4a875b42
Zhanghan Wang <hankzhwang@gmail.com>
2021-11-09 15:37:00 +0800

    clean up

--- hotspot/src/share/vm/interpreter/bytecodeInterpreter.cpp
+++ hotspot/src/share/vm/interpreter/bytecodeInterpreter.cpp
@@ -1035,7 +1035,6 @@ run:
 
         // print instruction if main is reached
         if (is_reach_main) {
-          tty->print_cr(CL_GREEN"%p %p %p"CNONE, (intptr_t*)pc, (intptr_t*)istate->bcp(), (intptr_t*)(pc - istate->bcp()));
           tty->print_cr(CL_YELLOW"================================================================="CNONE);
           tty->print_cr("current stack pointer %p %p %d", topOfStack, istate->stack_base(), GET_STACK_OFFSET);
           ConcolicMngr::ctx->print_stack_trace();
