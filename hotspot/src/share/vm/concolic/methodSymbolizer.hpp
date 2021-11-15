f55ec1dc894ae81b41df9beb215746caf4f32d01
Zhanghan Wang <hankzhwang@gmail.com>
2021-11-15 21:00:09 +0800

    more tidy methodSymbolizer

--- hotspot/src/share/vm/concolic/methodSymbolizer.hpp
+++ hotspot/src/share/vm/concolic/methodSymbolizer.hpp
@@ -6,6 +6,9 @@
 #include "concolic/exp/expression.hpp"
 
 #include <vector>
+#include <map>
+#include <set>
+#include <string>
 
 class ZeroFrame;
 
@@ -13,6 +16,7 @@ class MethodSymbolizer {
   typedef std::vector<Expression *> ParamList;
 
 private:
+  std::set<std::string> _symbolicMethods;
   ZeroFrame *_frame;
   ParamList _param_list;
 
@@ -21,6 +25,7 @@ public:
   void finish_method(ZeroFrame *caller_frame, ZeroFrame *callee_frame);
 
 private:
+  void invoke_method_helper(ZeroFrame *caller_frame, ZeroFrame *callee_frame);
   void reset();
 };
 
