cfd5ab44f890f3c104d148bbbab3e881f9dfe920
Zhiyuan Dong <windybeing@sjtu.edu.cn>
2021-11-25 16:32:30 +0800

    refine env scripts

--- scripts/init_env.sh
+++ scripts/init_env.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 # export MYJAVA_VARIENT="linux-x86_64-normal-zero-release"
-export MYJAVA_VARIENT="linux-x86_64-normal-zero-slowdebug"
-export MYJAVA_BUILD="$(pwd)/build/$MYJAVA_VARIENT"
-export MYJAVA_HOME="$MYJAVA_BUILD/jdk"
+export MYJAVA_VARIENT="linux-x86_64-normal-zero-release"
+export MYJAVA_BUILD="$(pwd)/build"
+export MYJAVA_HOME="$MYJAVA_BUILD/$MYJAVA_VARIENT/jdk"
+
+export SERVER_RELEASE_JAVA_HOME="$MYJAVA_BUILD/linux-x86_64-normal-server-release/jdk"
+export ZERO_RELEASE_JAVA_HOME="$MYJAVA_BUILD/linux-x86_64-normal-zero-release/jdk"
---
+++ scripts/unzip_debuginfo_zero_release.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+unzip -o $ZERO_RELEASE_JAVA_HOME/lib/amd64/server/libjvm.diz -d $ZERO_RELEASE_JAVA_HOME/lib/amd64/server/
