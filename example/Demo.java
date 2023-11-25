8439f577afcee62c8316156ec5085df584a62c8b
Novemser <novemser@gmail.com>
2023-11-25 10:13:53 +0800

    Modify readme

--- example/Demo.java
+++ example/Demo.java
@@ -1,22 +1,22 @@
 import java.util.*;
 
 public class Demo {
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         Integer val = new Integer(1);
         String str = new String("ipads");
 
-		// initialize the concolic execution, and symbolize the parameters that you like
-		System.startConcolic();
-		System.symbolize(val);
-		System.symbolize(str);
+        // initialize the concolic execution, and symbolize the parameters that you like
+        System.startConcolic();
+        System.symbolize(val);
+        System.symbolize(str);
         if (val == 3) {
-			System.out.println("Value = 3");
-		}
+            System.out.println("Value = 3");
+        }
 
-		if (str.startsWith("ip")) {
-			System.out.println("Str starts with ip");
-		}
+        if (str.startsWith("ip")) {
+            System.out.println("Str starts with ip");
+        }
         System.out.println("Path condition=" + System.getPathCondition());
         System.endConcolic();
-	}
+    }
 }
